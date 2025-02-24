8ddced8ee858d7ff7bf83546fbd4d43e9b80c9e0
recursivesanshine <alexsanlenz@gmail.com>
2025-02-24 13:13:36 +0000

    after flu

--- SmolVLM-Instruct-DeepSeek.py
+++ SmolVLM-Instruct-DeepSeek.py
@@ -37,7 +37,7 @@ def process_image(image_path):
     image_filename = os.path.splitext(os.path.basename(image_path))[0]
 
     # Create input messages
-    user_prompt = "Provide a detailed description of the image using exactly 128 characters.\nKeywords for Atmosphere:\nSerene\nNatural\nSketchy\nExpressive\nOutdoor\nKeywords for Emotion:\nTranquil\nReflective\nNostalgic\nDreamy\nPicture Elements:\nTrees\nFoliage\nWater\nSky\nSignature and Date"
+    user_prompt = "Give an objective description of the image"
     messages = [
         {
             "role": "user",
